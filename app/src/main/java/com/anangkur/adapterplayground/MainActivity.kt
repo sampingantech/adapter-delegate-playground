237a050748238359c4631288119be59aae61ab9f
anang <anangk97@gmail.com>
2020-10-08 16:33:08 +0700

    refactor(app): implement viewBinding in MainActivity.kt.

--- app/src/main/java/com/anangkur/adapterplayground/MainActivity.kt
+++ app/src/main/java/com/anangkur/adapterplayground/MainActivity.kt
@@ -2,10 +2,16 @@ package com.anangkur.adapterplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anangkur.adapterplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

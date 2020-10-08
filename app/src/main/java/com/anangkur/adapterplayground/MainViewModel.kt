215b2af90f5f7445dce666d7f212e26c1b6a5908
anang <anangk97@gmail.com>
2020-10-08 18:55:25 +0700

    feat(app): fixing sub adapter data.

--- app/src/main/java/com/anangkur/adapterplayground/MainViewModel.kt
+++ app/src/main/java/com/anangkur/adapterplayground/MainViewModel.kt
@@ -13,8 +13,19 @@ class MainViewModel: ViewModel() {
     fun createDummyDisplayableItems() {
         val displayableItems = ArrayList<DisplayableItem>()
         displayableItems.add(Banner("Covid-19", "resources", "Useful apps & more", ""))
-        displayableItems.add(Featured("Recenly updated", "Fresh features & content", emptyList()))
-        displayableItems.add(Promotion("New to rent", App("Google Play Movies & TV", "Google LLC", "", 4.0f, 4, false), emptyList()))
+        displayableItems.add(Promotion("New to rent", App("Google Play Movies & TV", "Google LLC", "", 4.0f, 4, false), listOf(
+            "", "", "", "", "", "", "", "", "", ""
+        )))
+        displayableItems.add(Promotion("Top selling ebooks on Play", App("Google Play Books - Ebooks, Audio", "Google LLC", "", 4.1f, 4, false), listOf(
+            "", "", "", "", "", "", "", "", "", ""
+        )))
+        displayableItems.add(Banner("Social impact stars", "", "Inspiring ideas", ""))
+        displayableItems.add(Featured("Recenly updated", "Fresh features & content", listOf(
+            App("Podcast App: Free & Offline Podcasts by ...", "Podcast", "", 5.0f, 27, false),
+            App("Air Brush: Easy Photo Editor", "Air Brush", "", 5.0f, 54, false),
+            App("Soccer Scores - FotMob", "Soccer Scores", "", 5.0f, 14, false),
+            App("Focus Plant - Stay Focused Study Timer", "Focus Plant", "", 5.0f, 50, false)
+        )))
         displayableItems.add(Campaign(listOf(
             Banner("Make stunning", "videos in seconds", "Explore templates", ""),
             Banner("Change colors", "in one tap", "Try AI Select", ""),
